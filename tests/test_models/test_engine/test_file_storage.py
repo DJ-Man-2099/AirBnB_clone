3e20090d6fb1fb854574d33a980f985ce237100b
DJ Man <49337662+DJ-Man-2099@users.noreply.github.com>
2023-12-16 07:47:06 +0200

    Task 1

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import unittest
+
+
+class TestFileStorage(unittest.TestCase):
+    """
+    Class for Unit Testing FileStorage()
+
+    this class checks for every detail in the class
+    """
